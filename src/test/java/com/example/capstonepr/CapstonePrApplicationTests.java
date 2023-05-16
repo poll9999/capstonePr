9ff11718b277f17065a80bbe6b4c34c79735581b
username459712 <94822739+username459712@users.noreply.github.com>
2023-05-16 17:47:13 +0900

    practice

---
+++ src/test/java/com/example/capstonepr/CapstonePrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstonepr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstonePrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
